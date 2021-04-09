fb3487aa76e4dc7fa1162c183c04df67e183d045
SeungHwan-Do <79446573+SeungHwan-Do@users.noreply.github.com>
2021-04-09 22:20:31 +0900

    Update Ex2-Doseunghwan.java

--- week2/Ex2-Doseunghwan.java
+++ week2/Ex2-Doseunghwan.java
@@ -10,7 +10,7 @@ public class Ex2 {
 	System.out.print("b: ");
 	b = sc.nextInt();
 	for(int i=a ;i<b+1;i++) {
-		System.out.print(i+"ÀÇ ¾à¼ö: ");
+		System.out.print(i+"의 약수 : ");
 		for(int j =1; j<=i;j++) {
 		if(i%j==0)
 			System.out.print(j+" ");
