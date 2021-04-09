2b1358caea2453c2640477b0efa819d7c016311f
SeungHwan-Do <79446573+SeungHwan-Do@users.noreply.github.com>
2021-04-09 21:13:41 +0900

    Add files via upload

---
+++ week2/WeakWater.java
@@ -0,0 +1,26 @@
+package hello;
+import java.util.Scanner;
+public class WeakWater {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+	int a, b;
+	System.out.print("a: ");
+	a = sc.nextInt();
+	System.out.print("b: ");
+	b = sc.nextInt();
+	for(int i=a ;i<b+1;i++) {
+		System.out.print(i+"ÀÇ ¾à¼ö: ");
+		for(int j =1; j<=i;j++) {
+		if(i%j==0)
+			System.out.print(j+" ");
+		}
+		System.out.println();
+		
+	}
+	
+	sc.close();
+
+	}
+
+}
